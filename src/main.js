0d8252f1239a998cded7212a70a6e4d65887e098
ahnndroid0001 <ahnndroid001@gmail.com>
2019-02-26 21:53:21 +0900

    [Vuex] 기본 설치 및 등록
    1) npm i vuex --save를 통해 Vuex 설치 -> package.json 통해 dependency 추가 여부 확인
    2) src/store/store.js 추가 -> 설치된 Vuex를 전역에서 사용 가능하도록 use 선언 및 Store 객체 export
    3) main.js에 store 모듈 등록 -> Vue 인스턴스 생성 시 import 해 온 store 모듈을 등록함으로써, 실제적으로 전역에서 Vuex 를 사용 가능하도록 설정

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+/* Vuex 사용을 위한 store 프로퍼티 등록 */
+import {
+  store
+} from './store/store'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store // import된 store 프로퍼티를 실제 전역으로 사용할 수 있도록 Vue 인스턴스 내 선언
 }).$mount('#app')
---
+++ src/store/store.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+/* 모든 전역에서 Vuex를 사용할 수 있도록 선언 */
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    // ...
+});
