71f216bf7468455eadaacdbe5cf78493028c9a19
ahnndroid0001 <ahnndroid001@gmail.com>
2019-02-26 16:02:24 +0900

    [UX 개선] Modal 팝업을 이용하기 위한 HTML 및 CSS 준비

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -4,14 +4,26 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
+
+    <!-- use the modal component, pass in the prop -->
+    <Modal v-if="showModal" @close="showModal = false">
+      <!--
+        you can use custom content here to overwrite
+        default content
+      -->
+      <h3 slot="header">custom header</h3>
+    </Modal>
   </div>
 </template>
 
 <script>
+import Modal from "./common/Modal.vue";
+
 export default {
   data: function() {
     return {
-      newTodoItem: ""
+      newTodoItem: "",
+      showModal: false
     };
   },
   methods: {
@@ -21,11 +33,15 @@ export default {
       if (this.newTodoItem !== "") {
         this.$emit("addTodoItem", this.newTodoItem);
         this.clearInput();
+      } else {
       }
     },
     clearInput: function() {
       this.newTodoItem = "";
     }
+  },
+  components: {
+    Modal
   }
 };
 </script>
